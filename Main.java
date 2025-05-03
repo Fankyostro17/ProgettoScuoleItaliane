7c8744e8b6e7a6539a4cb6c80e407da356ad1932
thomas fanciullacci <thomas.fanciullacci@gmail.com>
2025-05-03 17:24:26 +0200

    Sistemazione Output del Terminale e del Client

--- Main.java
+++ Main.java
@@ -1,21 +1,22 @@
+
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static final int PORT = 1717;
+
+    final static int PORT = 1717;
+    final static String serverAddress = "127.0.0.1";
 
     public static void main(String[] args) throws IOException {
-        do {
-            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-                System.out.println("EchoServer: started ");
-                System.out.println("Server Socket: " + serverSocket);
-                Socket clientSocket = serverSocket.accept();
-                Server server = new Server(clientSocket);
-                server.start();
-            } catch (Exception ex) {
-                System.out.println("error");
-            }
-        } while (true);
+        try (Socket clientSocket = new Socket(serverAddress, PORT)) {
+            System.out.println("Connessione al server: " + serverAddress + ":" + PORT);
+
+            Client client = new Client(clientSocket);
+            client.start();
+
+            client.join();
+        } catch (Exception e) {
+            System.out.println("Errore del Client.");
+        }
     }
 }
